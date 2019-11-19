714612395f52aec49b099a091bcd76e87f996c62
Marco Antonio <markinlimac@gmail.com>
2019-11-19 11:48:00 -0300

    Add test to get commit graph

---
+++ hubcare/hubcare_api/hubcare_api/tests/test_commit_graph.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from hubcare_api import views
+import os
+
+
+class CommitGraphTestCase(TestCase):
+    def setUp(self):
+        self.owner = 'markinlimac'
+        self.repo = 'dns_scan'
+        self.token_auth = str(os.getenv('GIT_AUTH_TOKEN'))
+        self.request_type = 'post'
+        self.metrics = views.get_metric(self.owner,
+                                        self.repo,
+                                        self.token_auth,
+                                        self.request_type)
+        self.expected = {
+                "x_axis": [
+                    "10 weeks ago",
+                    "9 weeks ago",
+                    "8 weeks ago",
+                    "7 weeks ago",
+                    "6 weeks ago",
+                    "5 weeks ago",
+                    "4 weeks ago",
+                    "3 weeks ago",
+                    "2 weeks ago",
+                    "this week"
+                ],
+                "y_axis": [
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0
+                ]
+            }
+
+    def test_get_indicators(self):
+
+        ans = views.get_commit_graph(self.metrics)
+        self.assertDictEqual(ans, self.expected)
